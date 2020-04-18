89507774b98514b1cff43f16bebfcf20de88506c
walison matheus <walisom360@hotmail.com>
2020-04-17 22:27:45 -0300

    all tests completed

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -1,5 +1,13 @@
 import Transaction from '../models/Transaction';
 
+interface CreateTransactionDto {
+  title: string;
+
+  value: number;
+
+  type: 'income' | 'outcome';
+}
+
 interface Balance {
   income: number;
   outcome: number;
@@ -9,20 +17,50 @@ interface Balance {
 class TransactionsRepository {
   private transactions: Transaction[];
 
+  private balance: Balance;
+
   constructor() {
     this.transactions = [];
+    this.balance = { income: 0, outcome: 0, total: 0 };
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balanceMap = this.transactions.reduce(
+      (accumulator, current) => {
+        return {
+          income:
+            current.type === 'income'
+              ? accumulator.income + current.value
+              : accumulator.income,
+          outcome:
+            current.type === 'outcome'
+              ? accumulator.outcome + current.value
+              : accumulator.outcome,
+        };
+      },
+      { income: 0, outcome: 0 },
+    );
+
+    const balance = {
+      ...balanceMap,
+      total: balanceMap.income - balanceMap.outcome,
+    };
+
+    this.balance = balance;
+
+    return this.balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, type, value }: CreateTransactionDto): Transaction {
+    const transaction = new Transaction({ title, type, value });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
